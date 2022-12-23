edc08cf020e4087281dff04ef8cb6438203917a7
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-12-23 10:36:17 -0600

    trabajos en inicio

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -9,21 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DummyController extends AbstractController
 {
     /**
-     * @Route("/dummy", name="app_dummy")
+     * @Route("/index", name="app_index")
      */
     public function index(): Response
     {
         return $this->render('dummy/index.html.twig', [
-            'controller_name' => 'DummyController',
-        ]);
-    }
-
-    /**
-     * @Route("/home", name="app_home")
-     */
-    public function home(): Response
-    {
-        return $this->render('dummy/home.html.twig', [
             'controller_name' => 'DummyController',
         ]);
     }
